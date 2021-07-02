c8028c28045fd595dbbd80e863a8f8a7f3b8fffb
Marcelo B <marcelobarros159@gmail.com>
2021-07-02 11:17:40 -0300

    questao 1001 - Extremamente Básico

---
+++ java/questao1001.java
@@ -0,0 +1,18 @@
+package questoes_uri;
+
+import java.util.Scanner;
+
+public class questao1001 {
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+
+		int X = A + B;
+
+		System.out.println("X = " + X);
+
+	}
+}
