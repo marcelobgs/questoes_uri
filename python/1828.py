e82b05fbb3bf8cbf8d7b9854dd67123c178fd38d
Marcelo Barros Galvão Segundo <marcelobarros159@gmail.com>
2023-10-17 10:22:08 -0300

    questão resolvida em python

--- python/1828.py
+++ python/1828.py
@@ -1,3 +1,30 @@
 T = int(input())
 for i in range(T):
-    print('teste')
+    x = input().split()
+    sheldon, raj = x
+    sheldon = (sheldon)
+    raj = (raj)
+    if sheldon == raj:
+        print(f'Caso #{i + 1}: De novo!')
+    elif sheldon == 'tesoura' and raj == 'papel':
+        print(f'Caso #{i + 1}: Bazinga!')
+    elif sheldon == 'papel' and raj == 'pedra':
+        print(f'Caso #{i + 1}: Bazinga!')
+    elif sheldon == 'pedra' and raj == 'lagarto':
+        print(f'Caso #{i + 1}: Bazinga!')
+    elif sheldon == 'lagarto' and raj == 'Spock':
+        print(f'Caso #{i + 1}: Bazinga!')
+    elif sheldon == 'Spock' and raj == 'tesoura':
+        print(f'Caso #{i + 1}: Bazinga!')
+    elif sheldon == 'tesoura' and raj == 'lagarto':
+        print(f'Caso #{i + 1}: Bazinga!')
+    elif sheldon == 'lagarto' and raj == 'papel':
+        print(f'Caso #{i + 1}: Bazinga!')
+    elif sheldon == 'papel' and raj == 'Spock':
+        print(f'Caso #{i + 1}: Bazinga!')
+    elif sheldon == 'Spock' and raj == 'pedra':
+        print(f'Caso #{i + 1}: Bazinga!')
+    elif sheldon == 'pedra' and raj == 'tesoura':
+        print(f'Caso #{i + 1}: Bazinga!')
+    else:
+        print(f'Caso #{i + 1}: Raj trapaceou!')
